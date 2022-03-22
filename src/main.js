baba8f2fdff50ae2b38c1a052ff9d7db521fe845
LiuKun <308880147@qq.com>
2022-03-22 11:13:35 +0800

    微调

--- src/main.js
+++ src/main.js
@@ -5,6 +5,16 @@
 import Vue from 'vue'
 import App from './app'
 import './assets/style.css'
+import figlet from 'figlet'
+import standard from 'figlet/importable-fonts/Standard.js'
+
+figlet.parseFont('Standard', standard)
+
+figlet.text('SVG    VUE JS', {
+  font: 'Standard',
+}, function(err, data) {
+  console.log(data)
+})
 
 new Vue({
   el: '#app',
